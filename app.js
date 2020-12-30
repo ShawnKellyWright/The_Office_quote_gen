830b1b5557c0ac43bd2d26b0f19124342905a4a3
Shawn Wright <shawncodewright@gmail.com>
2020-12-30 12:22:29 -0600

    fixed quotes loading, added emoji

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let quotesSeason1 = [
     'Michael: Any messages?   Pam: Uh, yeah. Just a fax.     Michael: Oh! Pam, this is from Corporate. How many times have I told you? There’s a special filing cabinet for things from corporate.     Pam: You haven’t told me.      Michael: It’s called the wastepaper basket! Look at that! Look at that face.',
     'Michael: People say I am the best boss. They go, “God we’ve never worked in a place like this before. You’re hilarious.” “And you get the best out of us.” (shows the camera his WORLD’S BEST BOSS mug) I think that pretty much sums it up. I found it at Spencer Gifts.'
 ];
-let new-quote = document.querySelector('.new-quote');
+let newQuote = document.querySelector('.newQuote');
 // new-quote.addEventListener('click', displayQuote);
 
 function displayQuote() {
@@ -13,11 +13,11 @@ function displayQuote() {
     // display the quote of that index
     let div = document.querySelector('#js-quote-text');
     let quote = `<div class="card">
-<img src="icons/favicon.png">
-<p>${quotes[index]}</p>
-<img src="icons/favicon.png">
+    <i class="fas fa-quote-left"></i>
+<p>${quotesSeason1[quotesRandom]}</p>
+<i class="fas fa-quote-right"></i>
 </div>
 `;
     div.innerHTML = quote;
 }
-new-quote.addEventListener('click', displayQuote;
+newQuote.addEventListener('click', displayQuote);
